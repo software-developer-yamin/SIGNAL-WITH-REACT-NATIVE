dc52eb9cd688d2fad5a9085cc623be2e68ddc2cc
DeveloperYamin <developer.yamin@gmail.com>
2022-03-15 21:25:52 +0600

    create CustomListItem component

---
+++ components/CustomListItem.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const CustomListItem = () => {
+  return (
+    <View>
+      <Text>CustomListItem</Text>
+    </View>
+  );
+};
+
+export default CustomListItem;
+
+const styles = StyleSheet.create({});
---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView></ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
