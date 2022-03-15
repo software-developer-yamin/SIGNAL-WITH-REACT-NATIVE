d81562dca26b9607216ce01509747d218095d443
DeveloperYamin <developer.yamin@gmail.com>
2022-03-15 10:57:42 +0600

    create login screen

--- App.jsx
+++ App.jsx
@@ -1,12 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet } from "react-native";
+import Login from "./screens/Login";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Login.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Login = () => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({});
