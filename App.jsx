44ab0bd2ffa22189d7c9e7ba7c28f9e99e6d4f24
DeveloperYamin <developer.yamin@gmail.com>
2022-03-15 11:12:35 +0600

    add global screenOptions style

--- App.jsx
+++ App.jsx
@@ -1,15 +1,21 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StyleSheet } from "react-native";
-import Login from "./screens/Login";
+import LoginScreen from "./screens/LoginScreen";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
 
+  const globalScreenOptions = {
+    headerStyle: { backgroundColor: "#2C6BED" },
+    headerTitleStyle: { color: "white" },
+    headerTintColor: "white",
+  };
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
